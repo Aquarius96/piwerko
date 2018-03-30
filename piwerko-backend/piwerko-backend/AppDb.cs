0d621a73087387ad726255350dc549575f5ab67c
Aquarius96 <https://Aquarius96@github.com>
2018-03-30 17:59:14 +0200

    dodanie mysql

---
+++ piwerko-backend/piwerko-backend/AppDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Jdforsythe.MySQLConnection;
+
+namespace MySqlConnector.Performance
+{
+    public class AppDb : IDisposable
+    {
+        public MySqlConnection Connection;
+
+        public AppDb()
+        {
+            Connection = new MySqlConnection("server=127.0.0.1;user id=root;password='';port=3306;database=blog;");
+        }
+
+        public void Dispose()
+        {
+            Connection.Close();
+        }
+    }
+}
