b9b48c9255bb439016f24f20c4493c9a208803cb
wrabel <dawidk.wrobel@gmail.com>
2018-04-01 17:52:13 +0200

    helo

--- piwerko-backend/piwerko-backend/Repo/AppDb.cs
+++ piwerko-backend/piwerko-backend/Repo/AppDb.cs
@@ -15,7 +15,7 @@ namespace Piwerko.Api.Repo
         public AppDb()
         {
             ConnectionMysql = new MySQLConn("server=127.0.0.1;user id=root;pwd='';database=billenium;");
-            ConnectionString = "Server=DESKTOP-6A001AV;;Initial Catalog=Test;Trusted_Connection=True";
+            ConnectionString = @"Server = (localdb)\mssqllocaldb; Database = billenium ;uid=root; Trusted_Connection = True; MultipleActiveResultSets = true;ConnectRetryCount = 0 ";//"Server =DESKTOP-6A001AV;;Initial Catalog=Test;Trusted_Connection=True";
             Issuer = "http://localhost:50977";
             Audience = "http://localhost:50977";
             Key = "VisualStudioDajeRakaTakBardzo!";
--- piwerko-backend/piwerko-backend/Startup.cs
+++ piwerko-backend/piwerko-backend/Startup.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Piwerko.Api.Repo;
 
 
