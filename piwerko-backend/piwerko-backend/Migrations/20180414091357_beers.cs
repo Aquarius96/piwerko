24581b523db7b7eb4a415f1a63a8473b4809db14
Aquarius96 <https://Aquarius96@github.com>
2018-04-14 14:48:22 +0200

    usunieto jedna z migracji

---
+++ piwerko-backend/piwerko-backend/Migrations/20180414091357_beers.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Piwerko.Api.Migrations
+{
+    public partial class beers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Beers",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    alcohol = table.Column<int>(nullable: false),
+                    breweryId = table.Column<int>(nullable: false),
+                    description = table.Column<string>(nullable: true),
+                    ibu = table.Column<int>(nullable: false),
+                    isConfirmed = table.Column<bool>(nullable: false),
+                    name = table.Column<string>(nullable: true),
+                    rating = table.Column<double>(nullable: false),
+                    servingTemp = table.Column<int>(nullable: false),
+                    type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Breweries",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    city = table.Column<string>(nullable: true),
+                    description = table.Column<string>(nullable: true),
+                    isConfirmed = table.Column<bool>(nullable: false),
+                    name = table.Column<string>(nullable: true),
+                    street = table.Column<string>(nullable: true),
+                    streetNumber = table.Column<int>(nullable: false),
+                    webUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Breweries", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rates",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    beerId = table.Column<int>(nullable: false),
+                    userId = table.Column<int>(nullable: false),
+                    value = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rates", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Beers");
+
+            migrationBuilder.DropTable(
+                name: "Breweries");
+
+            migrationBuilder.DropTable(
+                name: "Rates");
+        }
+    }
+}
