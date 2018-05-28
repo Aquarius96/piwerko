b4b8d4ed99ca4d7302adb2297cc9049a2a7df370
wrabel <dawidk.wrobel@gmail.com>
2018-05-28 23:42:24 +0200

    fix 3 XD

---
+++ piwerko-backend/piwerko-backend/Migrations/20180528214144_favoritev2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Piwerko.Api.Migrations
+{
+    public partial class favoritev2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Favorites",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    beer_id = table.Column<int>(nullable: false),
+                    user_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorites", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favorites");
+        }
+    }
+}
